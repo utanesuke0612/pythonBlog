8cca01a4ac48f5ed2e739ba90cd3f45caf540a85
utanesuke0612 <utanesuke@outlook.com>
2017-08-06 10:01:12 +0900

    add post list

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': post})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts': posts})
