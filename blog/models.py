17899623b943c1f1a0b790acbcc61b75479158e3
utanesuke0612 <utanesuke@outlook.com>
2017-08-19 13:10:22 +0900

    add default author

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
-    author = models.CharField(max_length=200)
+    author = models.CharField(default='lijun',max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
